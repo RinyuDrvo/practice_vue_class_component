ef2d5cb87ebefe68188fbe6743110a80a9844ff5
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-04-22 01:24:06 +0000

    mycomponent extend component base

--- src/component/MyComponent.ts
+++ src/component/MyComponent.ts
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import ComponentBase from './ComponentBase'
 
 @Component({
   template: require('./MyComponent.html') //html-loaderを使うと外部のhtmlファイルを読み込める
 })
-export default class MyComponent extends Vue {
+export default class MyComponent extends ComponentBase {
   // data
   hoge: string = 'Hello'
 
