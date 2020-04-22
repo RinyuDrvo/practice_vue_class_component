fbf6945403fa056cd17aa0eb65ef3e1f6509ad56
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-04-22 01:09:38 +0000

    make webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: 'development',
+  entry: __dirname + "/src/main.ts",
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' },
+      { test: /\.html$/, loader: 'html-loader?minimize=false' },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts'],
+    modules: [
+      "node_modules"
+    ],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+}
